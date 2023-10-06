a23c42990cf1f2ee243e4b4f8145c819e6f8cf53
Arthur Beleziak <artbele21@gmail.com>
2023-10-06 12:24:06 +0300

    removed weather controller added user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using DepDiary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepDiary.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DepDiaryContext depDiary;
+
+        public UserController(DepDiaryContext _depDiary)
+        {
+            depDiary = _depDiary;
+        }
+
+        [HttpGet]
+        public IActionResult GetUserList()
+        {
+            var userList = depDiary.Users.ToList();
+
+            return Ok(userList);
+        }
+
+    }
+}
